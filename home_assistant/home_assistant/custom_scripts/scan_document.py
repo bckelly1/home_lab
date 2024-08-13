ee495dc78f4d852faf55446310a1b005f22ed8a4
Brian Kelly <wolfyman06@gmail.com>
2024-08-12 19:22:22 -0600

    rewrite document scanner

--- home_assistant/home_assistant/custom_scripts/scan_document.py
+++ home_assistant/home_assistant/custom_scripts/scan_document.py
@@ -13,6 +13,9 @@ IMAGE_SCAN_DIR = os.environ.get('IMAGE_DIR', './')
 DOCUMENT_SCAN_DIR = os.environ.get('DOCUMENT_DIR', './')
 PAPERLESS_SCAN_DIR = os.environ.get('PAPERLESS_DIR', './')
 
+HOST_ADDRESS='192.168.2.19'
+JOBS_URL = "http://" + HOST_ADDRESS + "/Scan/Jobs"
+
 
 def image_backup(name, get_response):
     f = open(name, "wb")
@@ -31,51 +34,69 @@ def document_backup(name, get_response):
 
 
 def get_file_name(scan_type):
-    scan_type = scan_type.lower()
-    if scan_type == 'jpeg-pdf':
-        scan_type = 'jpeg'
-
     random = str(uuid.uuid4())
     name = "scanned-" + random + "." + scan_type
     print(name)
     return name
 
 
+def format_body_content(output_type, resolution):
+    resolution_values = {'high': {'width': 2550, 'length': 3300, 'resolution': 600}, 'low': {'width': 1500, 'length': 3000, 'resolution': 300}}
+    format = {'pdf': 'Pdf', 'jpeg': 'Jpeg'}
+    content_type = {'pdf': 'Document', 'jpeg': 'Photo'}
+    post_body = '<scan:ScanJob xmlns:scan="http://www.hp.com/schemas/imaging/con/cnx/scan/2008/08/19" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/" xmlns:fw="http://www.hp.com/schemas/imaging/con/firewall/2011/01/05"> <scan:XResolution>{{ resolution }}</scan:XResolution> <scan:YResolution>{{ resolution }}</scan:YResolution> <scan:XStart>0</scan:XStart> <scan:YStart>0</scan:YStart> <scan:Width>{{ width }}</scan:Width> <scan:Height>{{ height }}</scan:Height> <scan:Format>{{ format }}</scan:Format> <scan:CompressionQFactor>0</scan:CompressionQFactor> <scan:ColorSpace>Color</scan:ColorSpace> <scan:BitDepth>8</scan:BitDepth> <scan:InputSource>Platen</scan:InputSource> <scan:GrayRendering>NTSC</scan:GrayRendering> <scan:ToneMap> <scan:Gamma>1000</scan:Gamma> <scan:Brightness>1000</scan:Brightness> <scan:Contrast>1000</scan:Contrast> <scan:Highlite>179</scan:Highlite> <scan:Shadow>25</scan:Shadow> </scan:ToneMap> <scan:ContentType>{{ content_type }}</scan:ContentType> </scan:ScanJob>'
+
+    post_body.replace('{{ resolution }}', resolution_values[resolution]['resolution'])
+    post_body.replace('{{ width }}', resolution_values[resolution]['width'])
+    post_body.replace('{{ length }}', resolution_values[resolution]['length'])
+    post_body.replace('{{ format }}', format[output_type])
+    post_body.replace('{{ content_type }}', content_type[output_type])
+
+
+def send_job_request(output_type, resolution):
+    print("Running scan type " + output_type)
+    post_body = format_body_content(output_type, resolution)
+    post_response = requests.post(JOBS_URL, data=post_body, verify=False)
+    print(post_response)
+    location = post_response.headers["location"]
+    return location
+
+
+def get_printer_result(location):
+    job_id = location.split('/')[-1]
+    url = JOBS_URL + "/" + job_id + "/Pages/1"
+    return requests.get(url, verify=False)
+
+
+def save_file(destination, name, get_response):
+    if destination == 'docs':
+        document_backup(name, get_response)
+    elif destination == 'images':
+        image_backup(name, get_response)
+
+
 parser = argparse.ArgumentParser("scanner_runner")
-parser.add_argument("scan_type", help="What kind of scan will it be? pdf, jpeg, jpeg-pdf", type=str)
+parser.add_argument("-o", "output_type", help="What kind of scan will it be? pdf, jpeg", type=str)
+parser.add_argument("-d", "destination", help="Which folder will it go to? docs images", type=str)
+parser.add_argument("resolution", help="High or Low Resolution? high low", type=str)
 args = parser.parse_args()
 
-scan_type = args.scan_type.lower()
+output_type = args.output_type.lower()
+resolution = args.resolution.lower()
+destination = args.destination.lower()
 
-if scan_type == 'pdf':
-    print('Running a document scan..')
-    post_body = '<scan:ScanJob xmlns:scan="http://www.hp.com/schemas/imaging/con/cnx/scan/2008/08/19" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/" xmlns:fw="http://www.hp.com/schemas/imaging/con/firewall/2011/01/05"><scan:XResolution>300</scan:XResolution><scan:YResolution>300</scan:YResolution><scan:XStart>0</scan:XStart><scan:YStart>0</scan:YStart><scan:Width>2550</scan:Width><scan:Height>3300</scan:Height><scan:Format>Pdf</scan:Format><scan:CompressionQFactor>0</scan:CompressionQFactor><scan:ColorSpace>Color</scan:ColorSpace><scan:BitDepth>8</scan:BitDepth><scan:InputSource>Platen</scan:InputSource><scan:GrayRendering>NTSC</scan:GrayRendering><scan:ToneMap><scan:Gamma>1000</scan:Gamma><scan:Brightness>1000</scan:Brightness><scan:Contrast>1000</scan:Contrast><scan:Highlite>179</scan:Highlite><scan:Shadow>25</scan:Shadow></scan:ToneMap><scan:ContentType>Document</scan:ContentType></scan:ScanJob>'
-elif scan_type == 'jpeg' or scan_type == 'jpeg-pdf':
-    print('Running an image scan...')
-    post_body = '<scan:ScanJob xmlns:scan="http://www.hp.com/schemas/imaging/con/cnx/scan/2008/08/19" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/" xmlns:fw="http://www.hp.com/schemas/imaging/con/firewall/2011/01/05"><scan:XResolution>600</scan:XResolution><scan:YResolution>600</scan:YResolution><scan:XStart>0</scan:XStart><scan:YStart>0</scan:YStart><scan:Width>3000</scan:Width><scan:Height>5000</scan:Height><scan:Format>Jpeg</scan:Format><scan:CompressionQFactor>0</scan:CompressionQFactor><scan:ColorSpace>Color</scan:ColorSpace><scan:BitDepth>8</scan:BitDepth><scan:InputSource>Platen</scan:InputSource><scan:GrayRendering>NTSC</scan:GrayRendering><scan:ToneMap><scan:Gamma>1000</scan:Gamma><scan:Brightness>1000</scan:Brightness><scan:Contrast>1000</scan:Contrast><scan:Highlite>179</scan:Highlite><scan:Shadow>25</scan:Shadow></scan:ToneMap><scan:ContentType>Photo</scan:ContentType></scan:ScanJob>'
-else:
-    print(f'Unknown scan type! {scan_type}')
-    exit(-1)
 
-jobs_url = "http://192.168.2.19/Scan/Jobs"
-
-post_response = requests.post(jobs_url, data=post_body, verify=False)
-print(post_response)
-location = post_response.headers["location"]
+# Send the request, get the redirect response
+location = send_job_request(output_type, resolution)
 
 # A little too fast for the poor printer
 time.sleep(1)
 
-job_id = location.split('/')[-1]
-url = jobs_url + "/" + job_id + "/Pages/1"
-get_response = requests.get(url, verify=False)
+get_response = get_printer_result(location)
 
-name = get_file_name(scan_type)
+name = get_file_name(output_type)
 
-if scan_type == 'pdf' or scan_type == 'jpeg-pdf':
-    document_backup(name, get_response)
-elif scan_type == 'jpeg':
-    image_backup(name, get_response)
+save_file(destination, name, get_response)
 
 print('Finished scanning, cleaning files...')
 os.remove(name)
